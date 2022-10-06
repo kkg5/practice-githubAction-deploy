61f54b61ae9ba2b8de16199475332bd00b1c9863
kkg5 <kkg05102@gmail.com>
2022-10-06 15:08:12 +0900

    first commit

---
+++ src/main/java/com/codestates/practicegithubActiondeploy/PracticeDeployWithGithubActionApplication.java
@@ -0,0 +1,13 @@
+package com.codestates.practicegithubActiondeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeDeployWithGithubActionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeDeployWithGithubActionApplication.class, args);
+	}
+
+}
